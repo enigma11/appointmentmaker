c2f6c488dbb7c3e9468483f2857e1d5536b540c5
enigma11 <gdiamantidis@gmail.com>
2012-09-01 21:20:55 +0100

    added sample date to availability report

--- src/main/java/com/gdiama/domain/AvailabilityReport.java
+++ src/main/java/com/gdiama/domain/AvailabilityReport.java
@@ -2,12 +2,14 @@ package com.gdiama.domain;
 
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.Date;
 import java.util.Map;
 
 @Document
 public class AvailabilityReport {
 
     private Map<AppointmentCategory, Integer> availabilityPerCategory;
+    private final Date sampleDate = new Date();
 
     public AvailabilityReport(Map<AppointmentCategory, Integer> availabilityPerCategory) {
         this.availabilityPerCategory = availabilityPerCategory;
