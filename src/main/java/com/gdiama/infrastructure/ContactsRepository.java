11931095f10b440eeb3f824eff164a6f18dba327
enigma11 <gdiamantidis@gmail.com>
2012-09-04 20:59:22 +0100

    blah

--- src/main/java/com/gdiama/infrastructure/ContactsRepository.java
+++ src/main/java/com/gdiama/infrastructure/ContactsRepository.java
@@ -11,7 +11,6 @@ public class ContactsRepository {
     }
 
     public ContactDetails loadContactDetails () {
-        return new ContactDetails("george", "diamantidis", "gdiamantidis@gmail.com", "07909915700");
-//        return mongoDB.getMongoTemplate().findAll(ContactDetails.class).get(0);
+        return mongoDB.getMongoTemplate().findAll(ContactDetails.class).get(0);
     }
 }
